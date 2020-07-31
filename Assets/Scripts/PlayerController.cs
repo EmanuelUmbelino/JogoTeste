c07f8badaf84a77b5862f4a85b9d06e419a21ecc
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2020-07-31 14:52:09 -0300

    Iniciado Projeto de Jogo plataforma
    - Movimentanção
    - Pulo
    - Plataforma
    - Escadas
    - Agachar
    - Tiles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [System.Serializable]
+    public class JumpProperties
+    {
+        public float jumpForce = 9;
+
+        private bool _isGrounded;
+        public Transform groundCheck;
+        public float checkRadius = 0.3f;
+        public LayerMask whatIsGround;
+
+        private float jumpTimeCounter;
+        public float jumpTime = 0.2f;
+
+        private int extraJumps;
+        public int extraJumpsValue = 1;
+        private bool isJumping;
+
+        public void JumpUpdate(Rigidbody2D rb, Animator anim)
+        {
+            _isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+            if (_isGrounded == true) {
+                extraJumps = extraJumpsValue;
+                anim.SetBool("isJumping", false);
+            } else {
+                anim.SetBool("isJumping", true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0) {
+                anim.SetTrigger("takeOf");
+                isJumping = true;
+                jumpTimeCounter = jumpTime;
+                rb.velocity = Vector2.up * jumpForce;
+                extraJumps--;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space)) {
+                isJumping = false;
+            }
+
+            if (isJumping && jumpTimeCounter > 0) {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+
+        public bool isGrounded()
+        {
+            return _isGrounded;
+        }
+    }
+    public JumpProperties jumpProperties;
+
+    public float speed;
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D col;
+    private Animator anim;
+
+    private bool facingRight = true;
+
+    private bool isDown = false;
+    private float colSize;
+    private float colOffset;
+
+    public LayerMask whatIsLadder;
+    private bool isClimbing;
+
+    public float defaultGravityScale = 5;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        colSize = col.size.y;
+        colOffset = col.offset.y;
+    }
+
+    void FixedUpdate()
+    {
+        if (jumpProperties.isGrounded() && isDown) {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        } else {
+            rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+        }
+
+        if (isClimbing) {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        } else {
+            rb.gravityScale = defaultGravityScale;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & whatIsLadder) != 0)
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & whatIsLadder) != 0)
+        {
+            isClimbing = false;
+        }
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        
+        jumpProperties.JumpUpdate(rb, anim);
+
+        anim.SetBool("isRunning", inputHorizontal != 0);
+
+        if ((facingRight == false && inputHorizontal > 0) || (facingRight == true && inputHorizontal < 0)) {
+            Flip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            isDown = true;
+            col.size = new Vector2(col.size.x, colSize / 2);
+            col.offset = new Vector2(col.offset.x, colOffset * 2);
+            anim.SetBool("isDown", isDown);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            isDown = false;
+            col.size = new Vector2(col.size.x, colSize);
+            col.offset = new Vector2(col.offset.x, colOffset);
+            anim.SetBool("isDown", isDown);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        GetComponent<SpriteRenderer>().flipX = !facingRight;
+    }
+}
---
+++ Assets/Scripts/VerticalPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    private float waitTime;
+    private bool isGoingDown;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            if (waitTime <= 0) {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.1f;
+                isGoingDown = true;
+            }
+        }
+
+        if (waitTime <= 0 && isGoingDown == true) {
+            effector.rotationalOffset = 0;
+            isGoingDown = false;
+        } else {
+            waitTime -= Time.deltaTime;
+
+        }
+    }
+}
